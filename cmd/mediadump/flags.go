b58009c7ad494426b2215dffaff6aa688bf587f5
Jay Zhang <wangyoucao577@gmail.com>
2023-10-21 21:08:37 +0800

    feat: output formatted json by default

--- cmd/mediadump/flags.go
+++ cmd/mediadump/flags.go
@@ -22,7 +22,7 @@ var flags struct {
 func init() {
 	flag.StringVar(&flags.inputFilePath, "i", "", fmt.Sprintf("input file url, '%s' if stdin", util.InputStdin))
 	flag.StringVar(&flags.outputFilePath, "o", "", "output to file instead of stdout")
-	flag.StringVar(&flags.outputFormat, "of", dump.FormatJSON, fmt.Sprintf("output format, available values:%s", dump.FormatsHelper()))
+	flag.StringVar(&flags.outputFormat, "of", dump.FormatJSONFormatted, fmt.Sprintf("output format, available values:%s", dump.FormatsHelper()))
 
 	flag.BoolVar(&flags.parseES, "parse_es", false, "parse and dump Elementry Stream layer rather than container layer")
 
